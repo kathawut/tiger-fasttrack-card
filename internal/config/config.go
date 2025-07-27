c328b4cf7cb1c30eee0ba30a2f5447517eb6fdd6
คธาวุฒิ คำปอ <kathawut.p@gmail.com>
2025-07-27 21:54:20 +0700

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -25,13 +25,13 @@ func New() *Config {
 	return &Config{
 		Environment: getEnv("ENVIRONMENT", "production"),
 		Port:        getEnv("PORT", "8080"),
-		DatabaseURL: getEnv("DATABASE_URL", "postgresql://tiger-app:AVNS_c5wTtwqDbeQ2wWTmcTo@private-tiger-card-db-do-user-527620-0.g.db.ondigitalocean.com:25060/tiger-card?sslmode=verify-full"),
+		DatabaseURL: getEnv("DATABASE_URL", "postgresql://doadmin:AVNS_18zTR8noajdrYFr_rys@private-tiger-card-db-do-user-527620-0.g.db.ondigitalocean.com:25060/tiger-card?sslmode=verify-full"),
 		JWTSecret:   getEnv("JWT_SECRET", "79dbfcfcd07688b791aacd22fb797354"),
 		Database: DatabaseConfig{
 			Host:     getEnv("DB_HOST", "private-tiger-card-db-do-user-527620-0.g.db.ondigitalocean.com"),
 			Port:     getEnv("DB_PORT", "25060"),
-			User:     getEnv("DB_USER", "tiger-app"),
-			Password: getEnv("DB_PASSWORD", "AVNS_c5wTtwqDbeQ2wWTmcTo"),
+			User:     getEnv("DB_USER", "doadmin"),
+			Password: getEnv("DB_PASSWORD", "AVNS_18zTR8noajdrYFr_rys"),
 			Name:     getEnv("DB_NAME", "tiger-card"),
 			SSLMode:  getEnv("DB_SSLMODE", "verify-full"),
 		},
