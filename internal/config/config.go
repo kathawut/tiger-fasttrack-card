570803c415a12ab5820569f2c3bc772fa64e4dd9
Kathawut Kumpor <kathawut.p@gmail.com>
2025-07-28 09:06:09 +0700

    fix: deploy app platfrom

--- internal/config/config.go
+++ internal/config/config.go
@@ -25,9 +25,8 @@ func New() *Config {
 	return &Config{
 		Environment: getEnv("ENVIRONMENT", "production"),
 		Port:        getEnv("PORT", "8080"),
-		DatabaseURL: getEnv("DATABASE_URL", "postgresql://doadmin:AVNS_18zTR8noajdrYFr_rys@private-tiger-card-db-do-user-527620-0.g.db.ondigitalocean.com:25060/tiger-card?sslmode=verify-full"),
-		// JWTSecret:   getEnv("JWT_SECRET", "79dbfcfcd07688b791aacd22fb797354"),
-		JWTSecret: getEnv("JWT_SECRET", "your-production-jwt-secret"),
+		DatabaseURL: getEnv("DATABASE_URL", "postgres://doadmin:your-password@private-tiger-card-db-do-user-527620-0.g.db.ondigitalocean.com:25060/tiger-card?sslmode=require"),
+		JWTSecret:   getEnv("JWT_SECRET", "your-production-jwt-secret"),
 		Database: DatabaseConfig{
 			Host:     getEnv("DB_HOST", "private-tiger-card-db-do-user-527620-0.g.db.ondigitalocean.com"),
 			Port:     getEnv("DB_PORT", "25060"),
